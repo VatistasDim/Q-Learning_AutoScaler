5dad06d45e1f741ec20c727703c274183d0f229d
Vatistas Dimitris <95622182+VatistasDim@users.noreply.github.com>
2023-07-07 20:26:12 +0300

    Update cpu_metrics.py

--- Application_v_1/main_application/metrics/cpu_metrics.py
+++ Application_v_1/main_application/metrics/cpu_metrics.py
@@ -2,6 +2,8 @@ import requests
 import threading
 import time
 
+running = True
+
 def get_cpu_metrics():
     try:
         url = 'http://prometheus:9090/api/v1/query'
@@ -12,9 +14,16 @@ def get_cpu_metrics():
         data = response.json()
         if 'data' in data and 'result' in data['data']:
             results = data['data']['result']
-            for result in results:
-                metric_value = result['value']
-        return metric_value[1]
+            if results is not None:
+                for result in results:
+                    metric_value = result['value']
+                has_data = check_that_metrics_have_values(metric_value)
+                if has_data:
+                    return metric_value[1]
+                else:
+                    return has_data
+            else:
+                return None
     except Exception as e:
         print("An error occured:", e)
         
@@ -28,10 +37,16 @@ def get_memory_metrics():
         data = response.json()
         if 'data' in data and 'result' in data['data']:
             results = data['data']['result']
-            for result in results:
-                metric_value = result['value']
-                # print(f'Memory Usage: {metric_value[1]}')
-        return metric_value[1]
+            if results is not None:
+                for result in results:
+                    metric_value = result['value']
+                has_data = check_that_metrics_have_values(metric_value)
+                if has_data:
+                    return metric_value[1]
+                else:
+                    return has_data
+            else:
+                return None
     except Exception as e:
         print("An error occured:", e)
         
@@ -45,23 +60,36 @@ def get_service_up_time():
         data = response.json()
         if 'data' in data and 'result' in data['data']:
             results = data['data']['result']
-            for result in results:
-                metric_value = result['value']
-                # print(f'Memory Usage: {metric_value[1]}')
-        return metric_value[1]
+            if results is not None:
+                for result in results:
+                    metric_value = result['value']
+                has_data = check_that_metrics_have_values(metric_value)
+                if has_data:
+                    return metric_value[1]
+                else:
+                    return has_data
+            else:
+                return None
     except Exception as e:
         print("An error occured:", e)
 
+def check_that_metrics_have_values(metrics_value):
+    if metrics_value is None:
+        return None
+    else:
+        return metrics_value
 
-def fetch_metrics_periodically():
-    while True:
-        cpu_percent = get_cpu_metrics()
-        ram_percent = get_memory_metrics()
-        up_time = get_service_up_time()
+def fetch_metrics_periodically():    
+    cpu_percent = get_cpu_metrics()
+    ram_percent = get_memory_metrics()
+    up_time = get_service_up_time()
+    if cpu_percent is not None and ram_percent is not None and up_time is not None:
         print("CPU: " + cpu_percent + "% " + ("| RAM: ") + ram_percent + "% ")
-        # time.sleep(15)
+    else:
+        print("Metrics are not available just yet, please hold tight...")
+    
+    threading.Timer(10, fetch_metrics_periodically).start()
         
 if __name__ == '__main__':
-    
-    fetch_thread = threading.Thread(target=fetch_metrics_periodically)
-    fetch_thread.start()
+    print("Starting...")
+    fetch_metrics_periodically()
