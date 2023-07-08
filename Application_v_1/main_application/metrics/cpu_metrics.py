a41db31124f711575c4f0e0851d512eec46839c5
Vatistas Dimitris <95622182+VatistasDim@users.noreply.github.com>
2023-07-08 12:43:06 +0300

    Update cpu_metrics.py

--- Application_v_1/main_application/metrics/cpu_metrics.py
+++ Application_v_1/main_application/metrics/cpu_metrics.py
@@ -1,95 +1,90 @@
 import requests
 import threading
-import time
+import time, sched
 
 running = True
+s = sched.scheduler(time.time, time.sleep)
+url = 'http://prometheus:9090/api/v1/query'
+metric_value = None
 
 def get_cpu_metrics():
-    try:
-        url = 'http://prometheus:9090/api/v1/query'
-        params = {
+    params = {
             'query': "mnist_cpu_usage"
-        }
+         }
+    try:
         response = requests.get(url, params=params)
-        data = response.json()
-        if 'data' in data and 'result' in data['data']:
-            results = data['data']['result']
-            if results is not None:
-                for result in results:
-                    metric_value = result['value']
-                has_data = check_that_metrics_have_values(metric_value)
-                if has_data:
+        if response.status_code == 200:
+            data = response.json()
+            if data is not None and 'data' in data and 'result' in data['data']:
+                results = data['data']['result']
+                if results:
+                    for result in results:
+                        metric_value = result['value']
+                    
                     return metric_value[1]
-                else:
-                    return has_data
-            else:
-                return None
+                
+            return None
+        else:
+            return None
     except Exception as e:
-        print("An error occured:", e)
+        print("An error occurred during service uptime retrieval:", e)
+        return None
         
 def get_memory_metrics():
-    try:
-        url = 'http://prometheus:9090/api/v1/query'
-        params = {
+    params = {
             'query': "mnist_ram_usage"
-        }
+         }
+    try:
         response = requests.get(url, params=params)
-        data = response.json()
-        if 'data' in data and 'result' in data['data']:
-            results = data['data']['result']
-            if results is not None:
-                for result in results:
-                    metric_value = result['value']
-                has_data = check_that_metrics_have_values(metric_value)
-                if has_data:
+        if response.status_code == 200:
+            data = response.json()
+            if data is not None and 'data' in data and 'result' in data['data']:
+                results = data['data']['result']
+                if results:
+                    for result in results:
+                        metric_value = result['value']
+                    
                     return metric_value[1]
-                else:
-                    return has_data
-            else:
-                return None
+                
+            return None
+        else:
+            return None
     except Exception as e:
-        print("An error occured:", e)
+        print("An error occurred during service uptime retrieval:", e)
+        return None
         
 def get_service_up_time():
-    try:
-        url = 'http://prometheus:9090/api/v1/query'
-        params = {
+    params = {
             'query': "mnist_running_time"
-        }
+         }
+    try:
         response = requests.get(url, params=params)
-        data = response.json()
-        if 'data' in data and 'result' in data['data']:
-            results = data['data']['result']
-            if results is not None:
-                for result in results:
-                    metric_value = result['value']
-                has_data = check_that_metrics_have_values(metric_value)
-                if has_data:
+        if response.status_code == 200:
+            data = response.json()
+            if data is not None and 'data' in data and 'result' in data['data']:
+                results = data['data']['result']
+                if results:
+                    for result in results:
+                        metric_value = result['value']
+                    
                     return metric_value[1]
-                else:
-                    return has_data
-            else:
-                return None
+                
+            return None
+        else:
+            return None
     except Exception as e:
-        print("An error occured:", e)
-
-def check_that_metrics_have_values(metrics_value):
-    if metrics_value is None:
+        print("An error occurred during service uptime retrieval:", e)
         return None
-    else:
-        return metrics_value
 
-def fetch_metrics_periodically():    
+def fetch_metrics_periodically():
     cpu_percent = get_cpu_metrics()
     ram_percent = get_memory_metrics()
     up_time = get_service_up_time()
     if cpu_percent is not None and ram_percent is not None and up_time is not None:
-        print("CPU: " + cpu_percent + "% " + ("| RAM: ") + ram_percent + "% ")
+        print("CPU: " + cpu_percent + "% " + ("| RAM: ") + ram_percent + "% " + ("| Service_Run_Time: ") + up_time)
     else:
         print("Metrics are not available just yet, please hold tight...")
-    
-    threading.Timer(10, fetch_metrics_periodically).start()
         
-if __name__ == '__main__':
-    print("Starting...")
+while True:
     fetch_metrics_periodically()
+    time.sleep(0.5)
