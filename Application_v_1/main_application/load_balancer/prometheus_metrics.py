15318c1a6964c56fc5ba68a25b1b892c6fc0f840
Vatistas Dimitris <95622182+VatistasDim@users.noreply.github.com>
2024-05-27 18:48:26 +0300

    Update prometheus_metrics.py

--- Application_v_1/main_application/load_balancer/prometheus_metrics.py
+++ Application_v_1/main_application/load_balancer/prometheus_metrics.py
@@ -55,7 +55,7 @@ def get_service_up_time(url):
         return None
 
 def get_response_time(url):
-    params = {'query': 'sum(response_time{job="swarm-service"})'}
+    params = {'query': 'sum(flask_request_processing_time_seconds{job="swarm-service"})'}
     try:
         response = requests.get(url, params=params)
         if response.status_code == 200:
@@ -99,7 +99,6 @@ def fetch_metrics_periodically(url):
     if cpu_percent is not None and ram_percent is not None and up_time is not None and response_time is not None and cpu_shares is not None:
         return cpu_percent, ram_percent, up_time, response_time, cpu_shares
     else:
-        print(f'ERROR: There was an issue during the fetch of metrics. \n Relative logs may help. cpu_percent = {cpu_percent}, ram_percent = {ram_percent}, up_time = {up_time}, response_time = {response_time}, cpu_shares = {cpu_shares}')
         return None
 
 def start_metrics_service(url):
