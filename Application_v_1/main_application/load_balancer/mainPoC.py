94c08e0af2a789ac33432efc8cbd52dbb815d43f
Dimitrios Vatistas <vatistas.dim@gmail.com>
2025-09-15 21:24:16 +0300

    --

--- Application_v_1/main_application/load_balancer/mainPoC.py
+++ Application_v_1/main_application/load_balancer/mainPoC.py
@@ -60,22 +60,20 @@ def fetch_data():
     max_attempts = 100
     for attempt in range(max_attempts):
         try:
-            cpu_percent, ram_percent, time_up, response_time, cpu_shares = prometheus_metrics.start_metrics_service(url=PROM_URL)
-            if time_up != '0':
-                cpu_percent = int(float(cpu_percent))
-                ram_percent = int(float(ram_percent))
-                time_up = int(float(time_up))
-                response_time = float(response_time)
-                cpu_shares = calculate_cpu_shares(get_current_cpu_shares("mystack_application"))
-                if None in (cpu_percent, ram_percent, time_up, response_time, cpu_shares):
-                    continue
-                return cpu_percent, ram_percent, time_up, response_time, cpu_shares
+            cpu_percent, ram_percent, response_time, cpu_shares = prometheus_metrics.start_metrics_service(PROM_URL)
+            cpu_percent = int(float(cpu_percent))
+            ram_percent = int(float(ram_percent))
+            response_time = float(response_time)
+            cpu_shares = calculate_cpu_shares(get_current_cpu_shares("mystack_application"))
+            if None in (cpu_percent, ram_percent, response_time, cpu_shares):
+                continue
+            return cpu_percent, ram_percent, response_time, cpu_shares
         except Exception as e:
             print(f"Error: An error occurred during service metrics retrieval (Attempt {attempt + 1}/{max_attempts}):", e)
             if attempt < max_attempts - 1:
                 time.sleep(5)
     print("Failed to retrieve metrics after multiple attempts.")
-    return None, None, None, None, None
+    return None, None, None, None
 
 def apply_action(service_name, state, action, prometheus_url=None):
     k, u, c = state
--- Application_v_1/main_application/load_balancer/prometheus_metrics.py
+++ Application_v_1/main_application/load_balancer/prometheus_metrics.py
@@ -36,24 +36,6 @@ def get_memory_metrics(url):
         print("An error occurred during service RAM retrieval:", e)
         return None
 
-def get_service_up_time(url):
-    params = {'query': 'sum(running_time{job="swarm-service"})'}
-    try:
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            if data is not None and 'data' in data and 'result' in data['data']:
-                results = data['data']['result']
-                if results:
-                    metric_value = results[0]['value'][1]
-                    return metric_value
-            return None
-        else:
-            return None
-    except Exception as e:
-        print("An error occurred during service running retrieval:", e)
-        return None
-
 def get_response_time(url):
     params = {'query': 'avg(rate(json_endpoint_response_time_seconds_sum[10s]) / rate(json_endpoint_response_time_seconds_count[10s]))'}
     try:
@@ -93,11 +75,10 @@ def get_cpu_shares(url):
 def fetch_metrics_periodically(url):
     cpu_percent = get_cpu_metrics(url)
     ram_percent = get_memory_metrics(url)
-    up_time = get_service_up_time(url)
     response_time = get_response_time(url)
     cpu_shares = get_cpu_shares(url)
-    if cpu_percent is not None and ram_percent is not None and up_time is not None and response_time is not None and cpu_shares is not None:
-        return cpu_percent, ram_percent, up_time, response_time, cpu_shares
+    if cpu_percent is not None and ram_percent is not None and response_time is not None and cpu_shares is not None:
+        return cpu_percent, ram_percent, response_time, cpu_shares
     else:
         return None
 
