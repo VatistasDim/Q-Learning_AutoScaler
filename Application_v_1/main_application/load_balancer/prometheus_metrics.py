3e2ec7f554e8bfc9b500d29f5375730f3ef418b1
Dimitrios Vatistas <vatistas.dim@gmail.com>
2025-09-15 22:13:50 +0300

    swarm-service

--- Application_v_1/main_application/load_balancer/prometheus_metrics.py
+++ Application_v_1/main_application/load_balancer/prometheus_metrics.py
@@ -19,7 +19,7 @@ def get_cpu_metrics(url):
         return None
 
 def get_response_time(url):
-    params = {'query': 'avg(rate(json_endpoint_response_time_seconds_sum[10s]) / rate(json_endpoint_response_time_seconds_count[10s]))'}
+    params = {'query': 'json_endpoint_response_time_seconds_sum{job="swarm-service"} / #json_endpoint_response_time_seconds_count{job="swarm-service"} '}
     try:
         response = requests.get(url, params=params)
         if response.status_code == 200:
